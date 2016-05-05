465b5b068df12ff78f64b3741c0589c804df424f
sarahloh <sarahloh@Sarahs-MacBook-Pro.local>
2016-05-05 21:49:10 +0100

    Read json & add bootstrap

---
+++ js/cc.js
@@ -0,0 +1,10 @@
+$(function() {
+    $.getJSON("json/cc.json", function(data) {
+        var output = "<ul>";
+        for(var i in data.companies) {
+            output += '<li><a href="' + data.companies[i].url + '">' + data.companies[i].name + '</a></li>';
+        };
+        output += "</ul>";
+        $("#ccDiv").append(output);
+    });
+});
