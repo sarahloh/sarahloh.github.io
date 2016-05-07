6fa1f686538d1a41289d106eec57687521167021
sarahloh <sarahloh@Sarahs-MacBook-Pro.local>
2016-05-07 15:17:20 +0100

    Adding cc images

--- js/cc.js
+++ js/cc.js
@@ -2,12 +2,12 @@ $(function() {
     $.getJSON("data/cc.json", function(data) {
         var output = '<div class="row">';
         for(var i in data.companies) {
-                output += '<div class="col-md-2"><a href="' + 
-                            data.companies[i].url + 
-                            '" class="thumbnail">' + 
-                            '<img src="img/blue.jpg">' +
-                            '<div class="caption"><p>' + data.companies[i].name + '<p></div>' +
-                            '</a></div>';
+                output += '<div class="col-md-2">' + 
+                            '<a href="' + data.companies[i].url + '">' +
+                            '<div class="thumbnail">' + 
+                            '<img src="img/' + data.companies[i].name + '.png">' +
+                            '<div class="caption"><p>' + data.companies[i].name + '</p></div>' +
+                            '</div></a></div>';
         };
         output += "</div>";
         $("#ccDiv").append(output);
